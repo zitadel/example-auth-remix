e1855248b1f0cba55bced9963b0d2fafadceb109
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-27 17:02:08 +0300

    docs: add JSDoc on auth helpers + callbacks (parity with astro/nuxt/sveltekit/qwik)

--- knip.config.js
+++ knip.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  ignoreDependencies: [
-    '@commitlint/config-conventional'
-  ],
+  ignoreDependencies: ['@commitlint/config-conventional'],
   entry: ['app/routes/**/*', 'app/lib/**/*', 'app/auth.server.ts'],
   ignore: ['commitlint.config.js', '.react-router/**'],
 };
